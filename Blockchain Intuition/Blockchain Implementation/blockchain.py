903f5b017383204d713971fdab82ef4782d1f345
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-08-31 19:25:35 +0500

    Add previous block and create block functions

--- Blockchain Intuition/Blockchain Implementation/blockchain.py
+++ Blockchain Intuition/Blockchain Implementation/blockchain.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
@@ -9,3 +9,14 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain(-1)
