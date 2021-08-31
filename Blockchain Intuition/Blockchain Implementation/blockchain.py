03c561840d95f3ffcbfb07584a993b57cd61a64b
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-08-31 20:08:36 +0500

    Add chain validity check

--- Blockchain Intuition/Blockchain Implementation/blockchain.py
+++ Blockchain Intuition/Blockchain Implementation/blockchain.py
@@ -36,3 +36,20 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block["proof"]
+            proof = block["proof"]
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != "0000":
+                return False
+            previous_block = block
+            block_index += 1
+        return True
