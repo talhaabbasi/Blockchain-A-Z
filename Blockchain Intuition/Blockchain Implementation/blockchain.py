179a66f6d0e2cea31b439c3f176d50b86554907f
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-09-03 09:57:42 +0500

    Add mining block function

--- Blockchain Intuition/Blockchain Implementation/blockchain.py
+++ Blockchain Intuition/Blockchain Implementation/blockchain.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
+from werkzeug.wrappers import response
 
 
 class Blockchain:
@@ -53,3 +54,22 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
