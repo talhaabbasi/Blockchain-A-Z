bf1ff3a285e625201e922097b58057c596a33ec8
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-09-22 09:55:51 +0500

    Add replace_chain method

--- Cryptocurrency/Cryptocurrency Implementation/talhacoin.py
+++ Cryptocurrency/Cryptocurrency Implementation/talhacoin.py
@@ -76,6 +76,23 @@ class Blockchain:
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f"http://{node}/get_chain")
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
 
 app = Flask(__name__)
 blockchain = Blockchain()
