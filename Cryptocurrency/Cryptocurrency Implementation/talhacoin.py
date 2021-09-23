e15f38df0e018c734f3cc9e1d8d2a5b67ae61ead
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-09-23 09:48:42 +0500

    Add replace_chain method

--- Cryptocurrency/Cryptocurrency Implementation/talhacoin.py
+++ Cryptocurrency/Cryptocurrency Implementation/talhacoin.py
@@ -162,6 +162,18 @@ def connect_node():
     return jsonify(response), 201
 
 
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': "The node had different blocks so the chain was replaced by the longest one",
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': "All good. The blockchain is the largest one!",
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
+
+
 @app.route('/static/<path:path>')
 def send_static(path):
     return send_from_directory('static', path)
