8270cda76c10130c1b9fbe6a1d7678503df62314
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-09 10:46:16 +0500

    Add mappings

--- Smart Contracts/Smart Contracts Implementation/talhacoin_ico.sol
+++ Smart Contracts/Smart Contracts Implementation/talhacoin_ico.sol
@@ -2,7 +2,9 @@
 pragma solidity >=0.4.17;
 
 contract talhacoin_ico {
-    uint256 public max_talhacoins = 1000000;
-    uint256 public usd_to_talhacoins = 1000;
-    uint256 public total_talhacoins_bought = 0;
+    uint256 public max_talhacoin = 1000000;
+    uint256 public usd_to_talhacoin = 1000;
+    uint256 public total_talhacoin_bought = 0;
+    mapping(address => uint256) equity_talhacoin;
+    mapping(address => uint256) equity_usd;
 }
