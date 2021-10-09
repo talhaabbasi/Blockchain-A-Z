0a4dbcda2ba1ca66e9b623c3e842b311ed741306
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-10-09 10:37:27 +0500

    Basic setup

---
+++ Smart Contracts/Smart Contracts Implementation/talhacoin_ico.sol
@@ -0,0 +1,8 @@
+// Talhacoin ICO
+pragma solidity >=0.4.17;
+
+contract talhacoin_ico {
+    uint256 public max_talhacoins = 1000000;
+    uint256 public usd_to_talhacoins = 1000;
+    uint256 public total_talhacoins_bought = 0;
+}
